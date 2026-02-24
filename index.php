10b73fa78e0cf52be8e7ae072371a7300774e184
fyasmine <yasmine.fakir@ynov.com>
2026-02-24 14:43:00 +0100

    routing

--- index.php
+++ index.php
@@ -10,6 +10,31 @@ if ($path === '/movies') {
     $type = $_GET['type'] ?? 'popular';
     MovieController::list($type);
 } else {
+    if ($path === '/favorites' && $_SERVER['REQUEST_METHOD'] === 'POST') {
+
+        $input = file_get_contents('php://input');
+        $data = json_decode($input, true);
+    
+        if (!$data) {
+            http_response_code(400);
+            echo json_encode(["error" => "Données invalides"]);
+            exit;
+        }
+    
+        $favoritesFile = __DIR__ . '/favorites.json';
+    
+        $favorites = json_decode(file_get_contents($favoritesFile), true);
+        $favorites[] = $data;
+    
+        file_put_contents($favoritesFile, json_encode($favorites, JSON_PRETTY_PRINT));
+    
+        http_response_code(201);
+        echo json_encode([
+            "message" => "Film ajouté aux favoris",
+            "film" => $data
+        ]);
+        exit;
+    }
     http_response_code(404);
     echo json_encode(["error" => "Route inconnue"]);
 }
